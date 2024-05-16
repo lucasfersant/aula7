b6b1ef7424dd7854821052508754540aa2196e54
lucasfersant <lucas10fs3@gmail.com>
2024-05-16 18:03:12 -0300

    Add files via upload

---
+++ aula7.py
@@ -0,0 +1,83 @@
+# lista2 = [1,2,3,4,5,6,7,8,9,10]
+# #loop -> repetindo 
+# for numero in range(0,11):
+#    print(lista2[0], lista2[1], lista2[3])     
+
+# #iterar é percorrer uma lista 
+# lista2 +[1,2,3,4,5,6,7,8,9,10]
+# #loop->repetindo 
+# senha=123 
+# for numero in lista2:
+#    minha senha= int(input)('digete a senha:')
+# if senha == minha_senha:
+#    print('acesso liberado')
+
+
+# nome=input('digite seu nome')
+# usuarios= int(input('digite a quantidade de usuarios'))
+
+# for n in range(1, usuarios):
+#    nome = input('digite seu nome')
+# print('seja bem vindo(a)',nome, index)
+
+for n in range(1,6):
+    print('pessoas', n)
+nome=input('digite seu nome')
+print('seja bem vindo(a)'nome, index)
+
+
+
+
+carros=['ferrari','hb20,'fusca]
+concatene=carros + numeros 
+print(concatene)
+
+
+
+
+
+
+
+if 5 in numeros:
+    print('existe')
+else:
+    print('print não existe')
+    variavel=5 
+    for n in range(5,6):
+        x = int(input('digite'))
+
+
+#se condição == v:
+# faça isso 
+#senao 
+# faça isso aqui 
+        
+if 10 > 2:
+print('10 é maior')
+else:
+print('não é maior')
+
+
+
+
+
+
+import random 
+#numero = int(input('>>'))
+
+
+#aleatorio = random.random()
+aleatorio=random.randint(1,100)
+print(aleatorio)
+chute= int(input('digite um numero'))
+
+if chute >=
+
+
+
+
+
+
+
+
+
